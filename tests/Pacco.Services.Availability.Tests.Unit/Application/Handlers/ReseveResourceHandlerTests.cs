e6a627f5290ac78b95e5f48df3e9b428d1da8ba4
piotrwita <piotr.witaszewski@gmai.com>
2022-02-06 23:24:24 +0100

    Testowanie - Testy jednostkowe

---
+++ tests/Pacco.Services.Availability.Tests.Unit/Application/Handlers/ReseveResourceHandlerTests.cs
@@ -0,0 +1,82 @@
+﻿using NSubstitute;
+using Pacco.Services.Availability.Application.Commands;
+using Pacco.Services.Availability.Application.Commands.Commands;
+using Pacco.Services.Availability.Application.DTO;
+using Pacco.Services.Availability.Application.Exceptions;
+using Pacco.Services.Availability.Application.Srervices;
+using Pacco.Services.Availability.Application.Srervices.Clients;
+using Pacco.Services.Availability.Core.Entietes;
+using Pacco.Services.Availability.Core.Repositories;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Pacco.Services.Availability.Tests.Unit.Application.Handlers
+{
+    public class ReseveResourceHandlerTests
+    {
+        private Task Act(ReserveResource command) => _handler.HandleAsync(command);
+
+        [Fact]
+        public async Task given_invalid_resource_id_reserve_resource_should_throw_an_exception()
+        {
+            var command = new ReserveResource(Guid.NewGuid(),DateTime.UtcNow,0,Guid.NewGuid());
+
+            var exception = await Record.ExceptionAsync(async() => await Act(command));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<ResourceNotFoundException>(); 
+        }
+
+        [Fact]
+        public async Task given_valid_resource_id_for_valid_customer_reserve_resource_should_succeed()
+        {
+            var command = new ReserveResource(Guid.NewGuid(), DateTime.UtcNow, 0, Guid.NewGuid());
+            var resource = Resource.Create(new AggregateId(), new[] { "tag" });
+            
+            //w przypadku wywolania operacji get async powinien zwrocic nam ten obiekt resource
+            _resourcesRepository.GetAsync(command.ResourceId).Returns(resource);
+
+            var customerState = new CustomerStateDto
+            {
+                State = "valid"
+            };
+
+            _customersServiceClient.GetStateAsync(command.CustomerId).Returns(customerState);
+
+            await Act(command);
+
+            //otrzymalo
+            //jezeli bym nie mial resource w tym tescie to robie arg
+            //_resourcesRepository.Received().UpdateAsync(Arg.Is<Resource>(r => r.Id == command.ResourceId);
+            await _resourcesRepository.Received().UpdateAsync(resource);
+
+            //upewniamy sie ze nasz eventprocessor otrzymal na wejscie operacje processasync dla naszych resource events
+            await _eventProcessor.Received().ProcessAsync(resource.Events);
+        }
+            
+        // w xunit konstruktor i dispose jako typ czyszczenia
+        #region Arrange
+
+        private readonly ReseveResourceHandler _handler;
+        private readonly IResourcesRepository _resourcesRepository;
+        private readonly ICustomersServiceClient _customersServiceClient;
+        private readonly IEventProcessor _eventProcessor;
+
+        public ReseveResourceHandlerTests()
+        {
+            //dzieki temu ze tworzymy je przez substitute mozemy modelowac ich zachowanie i sterowac przeplywem flow w ramach wywolania handlera
+            _resourcesRepository = Substitute.For<IResourcesRepository>();
+            _customersServiceClient = Substitute.For<ICustomersServiceClient>();
+            _eventProcessor = Substitute.For<IEventProcessor>();
+
+            _handler = new ReseveResourceHandler(_resourcesRepository, _customersServiceClient, _eventProcessor);
+        }
+
+
+        #endregion
+    }
+}
---
+++ tests/Pacco.Services.Availability.Tests.Unit/Core/Entities/CreateResourceTests.cs
@@ -0,0 +1,63 @@
+﻿using Pacco.Services.Availability.Core.Entietes;
+using Pacco.Services.Availability.Core.Events;
+using Pacco.Services.Availability.Core.Exceptions;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Pacco.Services.Availability.Tests.Unit.Core.Entities
+{
+    public class CreateResourceTests
+    {
+        //arrange - przygotowanie calego srodowiska testowego
+        //setup ewentualnie stworzenie objektow
+
+        //act - kluczowa faza - wywolywanie logiki do przetestowania
+
+        //assert - czy wynik dzialania fazy act jest zgodny z naczymi oczekiwaniami
+
+        //przyjmuje dwie rzeczy ktore potrzebujemy do utworzenia konktretnego zasobu
+        private Resource Act(AggregateId id, IEnumerable<string> tags) => Resource.Create(id, tags);
+
+        [Fact] //że test
+        public void given_valid_id_and_tags_resource_should_be_created()
+        {
+            //Arrange
+            var id = new AggregateId();
+            var tags = new[] { "tag" };
+
+            //Act
+            var resource = Act(id, tags);
+
+            //Assert
+            resource.ShouldNotBeNull();
+            resource.Id.ShouldBe(id);
+            resource.Tags.ShouldBe(tags);
+
+            //sprawdzenie czy event sie dodal
+            resource.Events.Count().ShouldBe(1);
+
+            var @event = resource.Events.Single();
+            @event.ShouldBeOfType<ResourceCreated>();
+        }
+
+        [Fact]
+        public void given_empty_tags_resource_should_throw_an_exception()
+        {
+            //Arrange
+            var id = new AggregateId();
+            var tags = Enumerable.Empty<string>();
+
+            //Act
+            //oczekujemy wyjątku
+            var exception = Record.Exception(() => Act(id, tags));
+
+            //Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<MissingResourceTagsException>();
+        }
+    }
+}
